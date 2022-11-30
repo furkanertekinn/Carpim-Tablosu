0472a9c4555f398e32897869149c28f3ee69d606
Furkan Ertekin <furkanertekin0@gmail.com>
2022-11-30 13:56:18 +0300

    first commit

---
+++ ForDongusuCarpımTablo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForDongusuCarpımTablo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 1; i <=9 ; i++)
+            {
+                for (int j = 1; j <=9; j++)
+                {
+                    int carpimDeger = i * j;
+                    Console.Write("{0}*{1}={2}\t" ,i , j , carpimDeger);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
